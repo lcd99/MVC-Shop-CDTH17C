4432b4dc476922964aa3005d8459b663eb5e8d63
Lê Công Đức <lecongduc.1999@gmail.com>
2019-12-09 21:18:47 +0700

    Update Header.php

--- View/Shared/Header.php
+++ View/Shared/Header.php
@@ -1,3 +1,4 @@
+  
 <div id="header-home" class="header-home-abs">
 
     <!--khung header-top-->
@@ -5,27 +6,7 @@
 
         <!--logo-->
         <div class="logo" id="header-logo">
-            @call JS_Call_When_Onload('SSp.init')
-<div id="header-home" class="header-home-abs">
-
-    <!--khung header-top-->
-    <div id="header-top" class="header-top-abs">
-
-        <!--logo-->
-        <div class="logo" id="header-logo">
-        <img src="{{YUH_URI_ROOT}}/Resource/img/logo/3.png" href-click="{{YUH_URI_ROOT}}">
-        </div>
-
-        <!--Avatar người dùng và Menu-->
-        <div class="header-left" id="header-left">
-            @include "../Shared/Headers/User.php"
-            @include "../Shared/Headers/Navbar/Index.php"
-        </div>
-
-    </div>
-
-    @include "Slides.php"
-</div>
+            <img src="{{YUH_URI_ROOT}}/Resource/img/logo/3.png" href-click="{{YUH_URI_ROOT}}">
         </div>
 
         <!--Avatar người dùng và Menu-->
