213a780801a5640ac2c91fa8e803b5a94dcd0f2c
Lê Công Đức <lecongduc.1999@gmail.com>
2019-12-09 20:58:44 +0700

    Update HeaderHome.php

--- View/Home/HeaderHome.php
+++ View/Home/HeaderHome.php
@@ -1,3 +1,4 @@
+@call JS_Call_When_Onload('SSp.init')
 <div id="header-home" class="header-home-abs">
 
     <!--khung header-top-->
@@ -5,7 +6,7 @@
 
         <!--logo-->
         <div class="logo" id="header-logo">
-            <img src="{{YUH_URI_ROOT}}/Resource/img/logo/3.png" href-click="{{YUH_URI_ROOT}}">
+        <a href="{{YUH_URI_ROOT}}"><img src="{{YUH_URI_ROOT}}/Resource/img/logo/3.png"></a>
         </div>
 
         <!--Avatar người dùng và Menu-->
